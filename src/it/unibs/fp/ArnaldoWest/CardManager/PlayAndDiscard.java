e4856e3d78f7c979915fa6cbcae0831dfe1ecfa0
AlessandroMuscio <muscioalex30@gmail.com>
2024-06-07 12:22:21 +0200

    Implementata PlayAndDiscard

--- src/it/unibs/fp/ArnaldoWest/CardManager/PlayAndDiscard.java
+++ src/it/unibs/fp/ArnaldoWest/CardManager/PlayAndDiscard.java
@@ -1,4 +1,20 @@
 package it.unibs.fp.ArnaldoWest.CardManager;
 
-public class PlayAndDiscard {
+import it.kibo.fp.lib.AnsiColors;
+import it.unibs.fp.ArnaldoWest.main.UserInteraction;
+
+public class PlayAndDiscard extends Card{
+    public static final String CARD_WORDING = "Carta: ";
+    public static final String GITTATA_WORDING = "\nGittata: ";
+    private int distance;
+    public PlayAndDiscard(String name, int distance){
+        super(name);
+        this.distance = distance;
+    }
+    
+    @Override
+    public String toString() {
+        return UserInteraction.coloredMessage(CARD_WORDING, AnsiColors.PURPLE)  + getName() + UserInteraction.coloredMessage(GITTATA_WORDING, AnsiColors.PURPLE)
+                + distance;
+    }
 }
