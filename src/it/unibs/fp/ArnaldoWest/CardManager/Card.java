47b329ec6e9138e4ee4b8d53851308fe3c0f52f0
AlessandroMuscio <muscioalex30@gmail.com>
2024-06-07 12:22:08 +0200

    Implementata Card

--- src/it/unibs/fp/ArnaldoWest/CardManager/Card.java
+++ src/it/unibs/fp/ArnaldoWest/CardManager/Card.java
@@ -1,4 +1,15 @@
 package it.unibs.fp.ArnaldoWest.CardManager;
 
-public class Card {
+public abstract class Card {
+    private String name;
+
+    protected String getName() {
+        return name;
+    }
+
+    public Card(String name) {
+        this.name = name;
+    }
+
+    public abstract String toString();
 }
